0f996904e38f08f0cfb35211c2d3f717021a6033
FlexHaufen <ibims1k0tt@gmail.com>
2022-12-01 19:39:26 +0100

    Cleand up Lychee.h

--- src/Lychee/Lychee.h
+++ src/Lychee/Lychee.h
@@ -10,12 +10,15 @@
  */
 #pragma once
 
-//*** INCLUDES ***
-#include "Lychee/Core/Core.h"        // Main App
-#include "Lychee/Core/Log.h"         // Logger
+// *** INCLUDES ***
+// ** Main app **
+#include "Lychee/Core/Core.h"
+// ** Logger **
+#include "Lychee/Core/Log.h"        
 
 #include "Lychee/Core/Time/Deltatime.h"
 
+// ** Input & Types & Codes **
 #include "Lychee/Core/Input/Input.h"
 #include "Lychee/Helper/KeyCodes.h"
 #include "Lychee/Helper/MouseCodes.h"
@@ -26,10 +29,9 @@
 #include "Lychee/Renderer/Renderer2D.h"
 #include "Lychee/Renderer/RenderCommand.h"
 
-#include "Lychee/Renderer/Buffer.h"
 #include "Lychee/Renderer/Shader.h"
 #include "Lychee/Renderer/Texture.h"
-#include "Lychee/Renderer/VertexArray.h"
 
 #include "Lychee/Renderer/OrthographicCameraController.h"
+
 //*** DEFINES ***
